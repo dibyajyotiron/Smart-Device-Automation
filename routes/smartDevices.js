151e632d07987717117c5d3ab5b203dd706ee408
dibyajyoti_baqala <dibyajyoti.beingmyself.ghosal@gmail.com>
2020-02-17 03:24:29 +0530

    removed unwanted code

--- routes/smartDevices.js
+++ routes/smartDevices.js
@@ -3,10 +3,13 @@ const { validateReqBody } = require("../middlewares/generic");
 const { validateDevice } = require("../middlewares/smartDeviceMiddleware");
 const { auth } = require("../middlewares/userMiddleware");
 const { validateDeviceSchema } = require("../models/joiSchema");
-const { createOrUpdateDevice, getAllDevicesForARoom } = require("../controllers/smartDeviceController");
+const { createOrUpdateDevice, getAllDevicesForARoom, deleteDevice, updateDeviceDetails, updateDeviceStatus } = require("../controllers/smartDeviceController");
 
 router.use(auth);
 router.get("/:roomId", getAllDevicesForARoom);
 router.post("/create", validateReqBody(validateDeviceSchema), createOrUpdateDevice);
-router.put("/update/:id", validateDevice, createOrUpdateDevice);
+router.put("/update/:id", validateDevice, updateDeviceDetails);
+router.put("/update/:id/status", validateDevice, updateDeviceStatus);
+router.delete("/:id", validateDevice, deleteDevice);
+
 module.exports = router;
