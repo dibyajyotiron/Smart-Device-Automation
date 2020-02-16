b09d7490d725beb76813fe145b2fcdf04e6c7dc7
dibyajyoti_baqala <dibyajyoti.beingmyself.ghosal@gmail.com>
2020-02-17 03:23:11 +0530

    removed unwanted function

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,21 +1,6 @@
 const { Room } = require("../models/room");
 const url = require("url");
 
-const createDefaultRooms = async user => {
-    const roomNames = ["Home", "Bedroom", "Hall", "Kitchen"];
-    const roomsPromise = [];
-    let newRoom;
-    for (const room of roomNames) {
-        newRoom = new Room({
-            name: room,
-            user,
-        });
-        roomsPromise.push(newRoom);
-    }
-    await Promise.all(roomsPromise);
-    return "Default rooms added!";
-};
-
 module.exports = {
     async createOrUpdateRoom(req, res) {
         const { name } = req.body;
@@ -27,5 +12,4 @@ module.exports = {
         await room.save();
         return res.json({ success: true, message: `${name} added!` });
     },
-    async getAllRooms(req, res) {},
 };
